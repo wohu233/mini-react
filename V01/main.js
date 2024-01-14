9a5629f5de734b951ea76d45dfc5c7e20da05a2e
Ferris <autometarpa@163.com>
2024-01-14 11:36:28 +0800

    描述dom子节点

--- V01/main.js
+++ V01/main.js
@@ -1,8 +1,8 @@
-//V1
+//v1
 // 创建一个div元素
 const dom = document.createElement("div")
 // 设置该div元素的id为app
-dom.id="app"
+dom.id = "app"
 // 将该div元素添加到id为root的元素中
 document.querySelector('#root').append(dom)
 
@@ -10,3 +10,23 @@ document.querySelector('#root').append(dom)
 const textNode = document.createTextNode("")
 textNode.nodeValue = "app"
 dom.append(textNode)
+
+//v2 react → vdom → js object
+
+//dom结构是一颗树
+//如何描述子节点
+//type props chidren
+const textEl = {
+    type: "TEXT_ELEMENT",
+    props: {
+        nodeValue: "app",
+        children: [],
+    },
+}
+const el = {
+    type: "div",
+    props: {
+        id: "app",
+        children: [textEl],
+    },
+};
